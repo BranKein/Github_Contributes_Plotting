2f33a244d7bc60df26d056b8168d1f2d5f567ae0
Yeonhyuk <brankein13@gm.gist.ac.kr>
2021-05-07 00:03:59 +0900

    final commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, redirect, url_for
-from flask import render_template as raw_render_template
+from flask import Flask
+from flask import render_template
 from flask_cors import CORS
 from contributes import contribute_blueprint
 from htmlmin import minify
@@ -7,18 +7,13 @@ from htmlmin import minify
 app = Flask(__name__)
 CORS(app)
 
-app.secret_key = 'be646d0f29314d179b70c741b9e6fb49b904f83ad958c94d'
-
+app.secret_key = 'dsf654fds509654h654jyjhhg654ghj45tr489erew'
 app.register_blueprint(contribute_blueprint, url_prefix='/contribute')
-
-
-def render_template(*args, **kwargs):
-    return minify(raw_render_template(*args, **kwargs), remove_comments=True, remove_empty_space=True)
 
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    return minify(render_template('index.html'), remove_comments=True, remove_empty_space=True)
 
 
 if __name__ == '__main__':
